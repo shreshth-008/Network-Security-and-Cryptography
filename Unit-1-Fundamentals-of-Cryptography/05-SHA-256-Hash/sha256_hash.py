acc62df832eba76161d4b51606ae0ec558b11736
Shreshth Mishra <shreshthmishra58@gmail.com>
2026-09-08 21:49:55 +0530

    Add SHA-256 Implementation

---
+++ Unit-1-Fundamentals-of-Cryptography/05-SHA-256-Hash/sha256_hash.py
@@ -0,0 +1,24 @@
+import hashlib
+
+
+def calculate_sha256(message):
+    message_bytes = message.encode("utf-8")
+
+    sha256 = hashlib.sha256()
+
+    sha256.update(message_bytes)
+
+    hash_value = sha256.hexdigest()
+
+    return hash_value
+
+
+print("=== SHA-256 Hash Function ===")
+
+message = input("Enter message: ")
+
+hash_value = calculate_sha256(message)
+
+print("\nOriginal Message :", message)
+print("SHA-256 Hash     :", hash_value)
+print("Hash Length      :", len(hash_value), "characters")
