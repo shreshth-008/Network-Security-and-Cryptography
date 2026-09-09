d2e48e5604ba878babf73ef8eeb3341130a94d25
Shreshth Mishra <shreshthmishra58@gmail.com>
2026-09-09 21:31:10 +0530

    Add Message Authentication Code

---
+++ Unit-1-Fundamentals-of-Cryptography/06-Message-Authentication-Code/mac.py
@@ -0,0 +1,23 @@
+import hashlib
+
+
+def generate_mac(message, key):
+    data = key + message
+    data_bytes = data.encode("utf-8")
+
+    hash_object = hashlib.sha256(data_bytes)
+    mac = hash_object.hexdigest()
+
+    return mac
+
+
+print("=== Message Authentication Code (MAC) ===")
+
+message = input("Enter message: ")
+key = input("Enter secret key: ")
+
+mac = generate_mac(message, key)
+
+print("\nMessage   :", message)
+print("Secret Key:", key)
+print("MAC       :", mac)
